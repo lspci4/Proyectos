7606a88d9bd483dac71091d4390adfb199b894f9
lspci4 <ingluispt@gmail.com>
2025-07-08 16:46:16 -0400

    salida de variables normales

--- punteros/punteros.c
+++ punteros/punteros.c
@@ -1,5 +1,21 @@
-//1. variables normales
-//2. Punteros
 //3. Modificación a través de munteros
 //4. Aritmética de punteros
 //5. Puntero a puntero 
+
+#include <stdio.h>
+int main(){
+    //1. variables normales
+    int x = 10;
+    char y = 'X';
+    
+    //2. Punteros
+    int *p1 = &x;
+    char *p2 = &y;
+    
+    printf("=== VALORES ORIGINALES ===\n");
+    printf("valor x: %d (dirección de x: %p)\n", x, &x);
+    printf("valrr y: %c (dirección de y: %p)\n\n", y, &y);
+    
+    
+    return 0;
+}

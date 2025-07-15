f7bcdef8523a385da9911a1776de81d6d7ae75b4
lspci4 <ingluispt@gmail.com>
2025-07-14 23:34:23 -0400

    Agregada función switch

--- nota_examen/nota_examen.c
+++ nota_examen/nota_examen.c
@@ -1,5 +1,26 @@
 #include <stdio.h>
 
+/*Dada una nota de un examen mediante un código
+escribir el literal que le corresponde a la nota.*/ 
+
 int main(){
+    char nota;
+    printf("Introduzca calificación (A-F) y pulse Intro:");
+    scanf("%c", &nota);
+
+    switch (nota){
+        case 'A':   puts("Excelente. Examen superado");
+                    break;
+        case 'B':   puts("Notable. Suficiencia");
+                    break;
+        case 'C':   puts("Aprobado");
+                    break;
+        case 'D':   
+        case 'F':   puts("Suspendido");
+                    break;
+        default:    puts("No es posible esta nota");
     
+    puts("Final de programa");
+    return 0;
+    }
 }

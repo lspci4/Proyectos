966c00cab4aba474b3c54a2d0791bc07a33e9c7b
lspci4 <ingluispt@gmail.com>
2025-07-18 14:21:23 -0400

    Agregado tarea principal y objetivos funcionales

---
+++ arreglo_dinamico_modularizado/arreglo_dinamico_modularizado.c
@@ -0,0 +1,13 @@
+/*
+=== Tarea principal===
+Construir un arreglo dinámico en C, similar a un ArrayList de Java o vector de C++, 
+que pueda crecer automáticamente y permita insertar y eliminar elementos.
+
+=== Objetivos funcionales ===
+Inicializar un arreglo con una capacidad inicial.
+Insertar elementos.
+Redimensionar el arreglo automáticamente si se llena.
+Eliminar el último elemento.
+Imprimir el contenido del arreglo.
+Liberar memoria al finalizar.
+*/

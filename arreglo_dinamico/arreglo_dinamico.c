e5e16dd2fbb148c4cbbcfe838ce396777bb02144
lspci4 <ingluispt@gmail.com>
2025-07-17 22:58:16 -0400

    Agregado comentarios

--- arreglo_dinamico/arreglo_dinamico.c
+++ arreglo_dinamico/arreglo_dinamico.c
@@ -1,6 +1,5 @@
 /*
-arreglo_dinamico.c
-    Vamos a crear un arreglo que:
+    ==== Arreglo Dinamico ====
     Inicia con capacidad para 2 elementos
     Agrega 5 números uno por uno
     Duplica su tamaño cuando se llena
@@ -11,30 +10,32 @@ arreglo_dinamico.c
 #include <stdlib.h>
 
 int main(){
-    int capacidad = 2;
-    int elementos = 0;
-    int *arr= malloc(capacidad * sizeof(int));
+    int capacidad = 2; //capacidad Inicial
+    int elementos = 0; //cantidad de elemento actuales
+    int *arr= malloc(capacidad * sizeof(int)); // reserva memoria dinamica
 
+    // Agregar 5 elemento uno por uno
     for(int i=0; i < 5; i++){
+        // Si esta lleno duplicar la capacidad
         if(elementos == capacidad){
             capacidad *=2;
-            int* new_arr = realloc(arr, capacidad * sizeof(int)); 
+            int* new_arr = realloc(arr, capacidad * sizeof(int)); // redimensionar 
             if(new_arr == NULL){
                 printf("[-] Error de memoria..\n");
                 free(arr);
                 return 1;
             } 
-            arr = new_arr;
+            arr = new_arr; // actualizar el puntero original
         }
-        arr[elementos] = i;
-        elementos++;
+        arr[elementos] = i; // insertar elemento
+        elementos++; // aumentar tamaño actual
     }
     printf("Elementos del arreglo dinamico:\n");
     for(int i = 0; i < elementos; i++){
         printf("%d ", arr[i]);
     }
     printf("\n");
-    free(arr);
+    free(arr); // liberar memoria
     return 0;
 
 }

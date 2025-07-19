b2118387888dc3fdc8432b64199a8dd69a519310
lspci4 <ingluispt@gmail.com>
2025-07-18 23:38:20 -0400

    Inicialización del arreglo dinámico

--- arreglo_dinamico_modularizado/arreglo_dinamico_modularizado.c
+++ arreglo_dinamico_modularizado/arreglo_dinamico_modularizado.c
@@ -11,3 +11,26 @@ Eliminar el último elemento.
 Imprimir el contenido del arreglo.
 Liberar memoria al finalizar.
 */
+#include <stdio.h>
+#include <stdlib.h>
+
+// Prototipos
+void crear_arreglo(int capacidad);
+
+int main(){
+    // Inicializar arreglo
+    int capacidad = 2;
+
+    crear_arreglo(capacidad);
+    return 0;
+}
+
+void crear_arreglo(int capacidad){
+    int *arr= malloc(capacidad * sizeof(int));
+    if(arr == NULL){
+        printf("[-] Error asignando memoria\n");
+    }else{
+        printf("[+] Asignación de memoria exitosa\n");
+        free(arr);
+    }
+}

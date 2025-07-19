d914398e6c21d08af6f368d3e3cc70c27c403f24
lspci4 <ingluispt@gmail.com>
2025-07-19 17:00:52 -0400

    Agregado 2 desafiio

--- arreglo_dinamico_modularizado/arreglo_dinamico_modularizado.c
+++ arreglo_dinamico_modularizado/arreglo_dinamico_modularizado.c
@@ -4,33 +4,64 @@ Construir un arreglo dinámico en C, similar a un ArrayList de Java o vector de 
 que pueda crecer automáticamente y permita insertar y eliminar elementos.
 
 === Objetivos funcionales ===
-Inicializar un arreglo con una capacidad inicial.
-Insertar elementos.
-Redimensionar el arreglo automáticamente si se llena.
-Eliminar el último elemento.
-Imprimir el contenido del arreglo.
-Liberar memoria al finalizar.
+1 Inicializar un arreglo con una capacidad inicial.
+2 Insertar elementos.
+3 Redimensionar el arreglo automáticamente si se llena.
+4 Eliminar el último elemento.
+5 Imprimir el contenido del arreglo.
+6 Liberar memoria al finalizar.
 */
 #include <stdio.h>
 #include <stdlib.h>
 
 // Prototipos
-void crear_arreglo(int capacidad);
+int *crear_arreglo(int capacidad);
+int *insertar_elemento(int *arreglo, int *tamaño, int *capacidad, int valor);
 
 int main(){
     // Inicializar arreglo
     int capacidad = 2;
+    int tamaño = 0;
 
-    crear_arreglo(capacidad);
+    // Desafio 1
+    int *arreglo = crear_arreglo(capacidad);
+    if(arreglo == NULL){
+        return 1;
+    }
+
+    // Desafio 2 y 3
+    arreglo = insertar_elemento(&arreglo, &tamaño, &capacidad, 10);
+    arreglo = insertar_elemento(&arreglo, &tamaño, &capacidad, 20);
+    
+    printf("%d", arreglo);
     return 0;
 }
 
-void crear_arreglo(int capacidad){
-    int *arr= malloc(capacidad * sizeof(int));
-    if(arr == NULL){
+// crear arreglo y retornar puntero
+int *crear_arreglo(int capacidad){
+    int *arreglo= malloc(capacidad * sizeof(int));
+    if(arreglo == NULL){
         printf("[-] Error asignando memoria\n");
     }else{
-        printf("[+] Asignación de memoria exitosa\n");
-        free(arr);
+        printf("[+] Asignación de memoria exitosa con capacidad: %d\n", capacidad);
     }
+    return arreglo;
+}
+
+// Insertar elementos y redimiensionar arreglo
+int *insertar_elemento(int *arreglo, int *tamaño, int *capacidad, int valor){
+    if(*tamaño >= *capacidad){
+        int nueva_capacidad = (*capacidad) *2;
+        int *nuevo_arreglo = realloc(arreglo, nueva_capacidad * sizeof(int));
+        if(nuevo_arreglo == NULL){
+            printf("[-] No se puede redimensionar el arreglo\n");
+            return arreglo;
+        }
+        arreglo = nuevo_arreglo;
+        *capacidad = nueva_capacidad;
+        printf("[+] Arreglo redimiensionado a nueva capacidad: %d\n", *capacidad);
+    }
+    arreglo[*tamaño] = valor;
+    (*tamaño)++;
+    printf("Insertado: %d (tamaño actual: %d)\n", valor,*tamaño);
 }

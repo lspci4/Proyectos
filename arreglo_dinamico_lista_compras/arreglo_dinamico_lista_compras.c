4c8efedf93d000007389f0130a48311e67084b6c
lspci4 <ingluispt@gmail.com>
2025-07-21 23:40:14 -0400

    Bolsa creada sin productos

--- arreglo_dinamico_lista_compras/arreglo_dinamico_lista_compras.c
+++ arreglo_dinamico_lista_compras/arreglo_dinamico_lista_compras.c
@@ -1,6 +1,19 @@
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(){
-    
+    int capacidad = 2; // Espacio para 2 productos en la bolsa
+    int num_productos = 0; // Productos añadidos
+
+    // Creo la bolsa vacia
+    char *lista = malloc(capacidad * sizeof(int));
+    if(lista == NULL){
+        printf("No hay espacio en la bolsa\n");
+        return 1;
+    }
+
+    printf("Bolsa de compras creada\n");
+
+
     return 0;
 }
